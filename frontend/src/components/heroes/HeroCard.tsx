7f4d414132997599d172df2ca68a8db70005f14f
Maksym <qlowlight@gmail.com>
2026-01-15 18:56:06 +0200

    add getAllHeroes

--- frontend/src/components/heroes/HeroCard.tsx
+++ frontend/src/components/heroes/HeroCard.tsx
@@ -1,13 +1,13 @@
 import {Link} from "react-router-dom";
 
 type HeroCardType = {
-    name: string;
+    nickname: string;
     imgUrl: string;
     id: number;
 }
 
 
-export function HeroCard({id, name, imgUrl}: HeroCardType) {
+export function HeroCard({id, nickname, imgUrl}: HeroCardType) {
     return (
         <Link to={`/heroes/${id}`}>
 
@@ -15,14 +15,14 @@ export function HeroCard({id, name, imgUrl}: HeroCardType) {
                 <div className="aspect-[4/3] w-full overflow-hidden bg-gray-100">
                     <img
                         src={imgUrl}
-                        alt={name}
+                        alt={nickname}
                         className="h-full w-full object-cover"
                         loading="lazy"
                     />
                 </div>
 
                 <figcaption className="mx-auto p-3 text-bas font-semibold text-center truncate">
-                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis ea ipsam laboriosam nobis pariatur quis quo saepe! Aspernatur atque autem beatae blanditiis deleniti doloribus dolorum ea eligendi enim esse exercitationem facere fugit harum illo ipsa iure mollitia nam neque placeat quidem rem reprehenderit sapiente tempore, temporibus vero. A accusantium animi, eos excepturi facilis laboriosam, necessitatibus nihil officia possimus provident quas quasi reiciendis sequi sit suscipit temporibus tenetur voluptate! Impedit necessitatibus rem saepe? Ab accusantium adipisci aut blanditiis dolorum earum eligendi laboriosam numquam obcaecati omnis porro qui ratione similique, soluta sunt suscipit tenetur! Cupiditate dolorem doloribus dolorum eius eligendi error expedita fuga illo impedit inventore ipsam, ipsum itaque minima modi nemo neque nihil nisi obcaecati odio praesentium provident, quaerat quo reprehenderit saepe sint suscipit tempora tenetur velit voluptatem voluptates! A aspernatur at consequuntur cumque deleniti, dignissimos ducimus ea earum incidunt iure, libero minus molestias officia optio placeat porro quia repellat rerum sint voluptatem? Animi aperiam debitis deserunt eum eveniet facere ipsum magni molestiae odit omnis quidem quis quo quod ratione reiciendis repellat saepe suscipit temporibus, tenetur veniam vitae voluptas voluptates! Consectetur dolorum iusto praesentium repudiandae. Aliquam consectetur consequuntur debitis deserunt labore laborum, nesciunt, numquam placeat porro, quia soluta vitae voluptatibus.</span>
+                    <span>{nickname}</span>
                 </figcaption>
             </figure>
         </Link>
--- frontend/src/pages/HeroesListPage.tsx
+++ frontend/src/pages/HeroesListPage.tsx
@@ -1,17 +1,31 @@
 import HeroCard from "../components/heroes/HeroCard.tsx";
-
-const arr = Array.from({length: 400});
+import {useAppDispatch, useAppSelector} from "../store/hooks.ts";
+import {getHeroes, type Hero} from "../store/heroesSlice.ts";
+import {useEffect} from "react";
 
 
 function HeroesListPage() {
+
+    const heroes:Hero[] = useAppSelector((state) => state.heroes.items);
+    const isLoading = useAppSelector((state) => state.heroes.isLoading);
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(getHeroes())
+    },[dispatch]);
+
+
+   if(isLoading) return <div>LOADING</div>
+
     return (
         <div>
             <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                {arr.map((_, i) => <HeroCard
-                    key={i}
-                    id={i}
-                    imgUrl={'https://www.dexerto.com/cdn-image/wp-content/uploads/2024/02/21/all-might-my-hero-academia.jpg?width=1200&quality=60&format=auto'}
-                    name={'test'}/>)}
+                {heroes.map((item:Hero) => <HeroCard
+                    key={item.id}
+                    id={item.id}
+                    imgUrl={item.imageUrl}
+                    nickname={item.nickname}/>)}
             </div>
 
         </div>
