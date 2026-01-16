b0e10d7b59499c5ce86f894a8f52e5c26f127710
Maksym <qlowlight@gmail.com>
2026-01-16 18:28:13 +0200

    add superheroes service and repositories

--- backend/src/modules/superheroes/superheroes.module.ts
+++ backend/src/modules/superheroes/superheroes.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SuperheroesController } from './api/superheroes.controller';
+import { SuperheroesRepository } from './infrastructure/superheroes.repository';
+import { SuperheroesQueryRepository } from './infrastructure/superheroes.query-repository';
+import { SuperheroesService } from './domain/superheroes.service';
 
 @Module({
   controllers: [SuperheroesController],
+  providers: [
+    SuperheroesService,
+    SuperheroesRepository,
+    SuperheroesQueryRepository,
+  ],
 })
 export class SuperheroesModule {}
