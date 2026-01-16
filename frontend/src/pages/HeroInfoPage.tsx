a975a2a98b3fb4009211fb3d19e9674a5325b18f
Maksym <qlowlight@gmail.com>
2026-01-16 10:46:56 +0200

    add ConfirmModal for deleting hero

--- frontend/src/pages/HeroInfoPage.tsx
+++ frontend/src/pages/HeroInfoPage.tsx
@@ -1,19 +1,32 @@
-import {Link, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import Button from "../components/ui/Button.tsx";
 import {useAppDispatch, useAppSelector} from "../store/hooks.ts";
 import {getHeroById} from "../store/heroesSlice.ts";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {ConfirmModal} from "../components/ui/ConfirmModal.tsx";
 
 function HeroesInfoPage() {
 
-
+    const [open, setOpen] = useState<boolean>(false)
     const {id} = useParams()
     const heroId = Number(id)
+
+    const navigate = useNavigate();
 
     const {isLoading,selected} = useAppSelector((state) => state.heroes);
 
 
     const dispatch = useAppDispatch();
+
+    const onDelete = async () => {
+        if(!selected) return;
+
+        console.log('delete')
+
+        setOpen(false)
+        navigate('/heroes')
+
+    }
 
 
    useEffect(() => {
@@ -29,6 +42,7 @@ function HeroesInfoPage() {
     return (
 
 
+
         <div className="space-y-4">
             <nav className="flex items-center justify-between">
                 <Link to="/heroes" className="inline-block text-blue-600 hover:underline">
@@ -38,11 +52,22 @@ function HeroesInfoPage() {
                     <Button type='button' disabled={isLoading} variant='secondary' className='mx-1.5'>
                         Edit
                     </Button>
-                    <Button type='button' disabled={isLoading} variant='danger'>
+                    <Button type='button' onClick={() => setOpen(true)} disabled={isLoading} variant='danger'>
                         Delete
                     </Button>
                 </div>
             </nav>
+
+            <ConfirmModal
+                open={open}
+                title="Delete the Hero?"
+                description="This action cannot be undone."
+                confirmText="Delete"
+                cancelText="Cancel"
+                loading={isLoading}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+            />
 
 
             <div className="grid gap-6 md:grid-cols-[280px_1fr] rounded-xl border bg-white p-4">
