f4720202d486b0cd9a5adf4376e431723da3758f
Maksym <qlowlight@gmail.com>
2026-01-16 18:49:21 +0200

    connected db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuperheroesModule } from './modules/superheroes/superheroes.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SuperheroesModule],
+  imports: [
+    SuperheroesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5420,
+        username: 'admin',
+        password: 'qwerty',
+        database: 'superheroes',
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
