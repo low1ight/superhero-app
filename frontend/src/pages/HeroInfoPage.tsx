1755686e1488f9b9444959cd02e29f498d29b088
Maksym <qlowlight@gmail.com>
2026-01-16 13:50:33 +0200

    refactoring HeroInfoPage

--- frontend/src/pages/HeroInfoPage.tsx
+++ frontend/src/pages/HeroInfoPage.tsx
@@ -4,10 +4,13 @@ import {useAppDispatch, useAppSelector} from "../store/hooks.ts";
 import {getHeroById} from "../store/heroesSlice.ts";
 import {useEffect, useState} from "react";
 import {ConfirmModal} from "../components/ui/ConfirmModal.tsx";
+import HeroInfo from "../components/heroes/HeroInfo.tsx";
+import HeroInfoSkeleton from "../components/heroes/HeroInfoSekeleton.tsx";
 
 function HeroesInfoPage() {
 
     const [open, setOpen] = useState<boolean>(false)
+
     const {id} = useParams()
     const heroId = Number(id)
 
@@ -35,13 +38,7 @@ function HeroesInfoPage() {
 
 
 
-   if(isLoading) return <div>LOADING</div>
-   if(!selected) return <div>404 :(</div>
-
-
     return (
-
-
 
         <div className="space-y-4">
             <nav className="flex items-center justify-between">
@@ -58,46 +55,25 @@ function HeroesInfoPage() {
                 </div>
             </nav>
 
-            <ConfirmModal
-                open={open}
-                title="Delete the Hero?"
-                description="This action cannot be undone."
-                confirmText="Delete"
-                cancelText="Cancel"
-                loading={isLoading}
-                onClose={() => setOpen(false)}
-                onConfirm={onDelete}
-            />
 
 
-            <div className="grid gap-6 md:grid-cols-[280px_1fr] rounded-xl border bg-white p-4">
+                <ConfirmModal
+                    open={open}
+                    title="Delete the Hero?"
+                    description="This action cannot be undone."
+                    confirmText="Delete"
+                    cancelText="Cancel"
+                    loading={isLoading}
+                    onClose={() => setOpen(false)}
+                    onConfirm={onDelete}
+                />
 
-                <div className="self-start h-fit overflow-hidden rounded-xl border bg-gray-100">
-                    <img
-                        src={selected.imageUrl}
-                        alt={selected.nickname}
-                        className="h-60 w-full object-cover block"
-                    />
-                </div>
+            {isLoading ?
+                <HeroInfoSkeleton/> :
+                selected ?
+                    <HeroInfo {...selected} /> :
+                    <div>404 :(</div>}
 
-
-                <div>
-                    <h1 className="text-2xl font-bold">{selected.nickname}</h1>
-                    <p className="mt-1 text-gray-600">
-                        <span className="font-medium">Real name:</span> {selected.realName}
-                    </p>
-
-                    <div className="mt-4">
-                        <h2 className="text-lg font-semibold">Description:</h2>
-                        <p className="mt-1 text-gray-700">{selected.description}</p>
-                    </div>
-
-                    <div className="mt-4">
-                        <h2 className="text-lg font-semibold">Power:</h2>
-                        <p className="mt-1 text-gray-700">{selected.superPower}</p>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 
