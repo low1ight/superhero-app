7f20723065140e2dd7f208cc2101ada992fe25e3
Maksym <qlowlight@gmail.com>
2026-01-17 15:36:31 +0200

    remove temp data, add real request

--- frontend/src/store/heroesSlice.ts
+++ frontend/src/store/heroesSlice.ts
@@ -1,80 +1,9 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import type {SuperheroFullInfoType} from "../shared/types/superhero-full-info.type.ts";
 import type {SuperheroSummaryType} from "../shared/types/superhero-summary.type.ts";
+import {superheroesApi} from "../shared/api/superheroes.api.ts";
 
 
-export const heroesFull:SuperheroFullInfoType[] = [
-    {
-        id: 1,
-        nickname: "Spider-Man",
-        realName: "Peter Parker",
-        description: "Friendly neighborhood hero.",
-        superPower: "Spider-sense, agility, web shooting.",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Spider-Man",
-    },
-    {
-        id: 2,
-        nickname: "All Might",
-        realName: "Toshinori Yagi",
-        description: "Symbol of Peace.",
-        superPower: "One For All — incredible strength.",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=All+Might",
-    },
-    {
-        id: 3,
-        nickname: "Batman",
-        realName: "Bruce Wayne",
-        description: "Dark Knight of Gotham.",
-        superPower: "Peak human abilities, intelligence, gadgets.",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Batman",
-    },
-    {
-        id: 4,
-        nickname: "Iron Man",
-        realName: "Tony Stark",
-        description: "Genius inventor in a high-tech suit.",
-        superPower: "Powered armor, flight, advanced weapons.",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Iron+Man",
-    },
-    {
-        id: 5,
-        nickname: "Wonder Woman",
-        realName: "Diana Prince",
-        description: "Amazon warrior and protector of justice.",
-        superPower: "Super strength, combat skill, lasso of truth.",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Wonder+Woman",
-    },
-];
-
-
-// 2) Короткі дані (тільки id, nickname, imageUrl) — теж 5 елементів
-export const heroesShort:SuperheroSummaryType[] = [
-    {
-        id: 1,
-        nickname: "Spider-Man",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Spider-Man",
-    },
-    {
-        id: 2,
-        nickname: "All Might",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=All+Might",
-    },
-    {
-        id: 3,
-        nickname: "Batman",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Batman",
-    },
-    {
-        id: 4,
-        nickname: "Iron Man",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Iron+Man",
-    },
-    {
-        id: 5,
-        nickname: "Wonder Woman",
-        imageUrl: "https://placehold.co/1200x1200/111827/FFFFFF/png?text=Wonder+Woman",
-    },
-];
 type HeroesState = {
     isLoading: boolean,
     items: SuperheroSummaryType[],
@@ -130,21 +59,13 @@ const heroesSlice = createSlice({
 
 export const heroesReducer = heroesSlice.reducer;
 
-const delay = (ms: number) => new Promise<void>((res) => setTimeout(res, ms));
-
 export const getHeroes = createAsyncThunk<SuperheroSummaryType[]>(
     "heroes/getAll",
-    async () => {
-            await delay(2010)
-            return heroesShort
-        });
+    async () => await superheroesApi.getAll());
 
 export const getHeroById = createAsyncThunk<SuperheroFullInfoType | null, number>(
     "heroes/getById",
-    async (id:number) => {
-        await delay(2100)
-        return heroesFull.find((item:SuperheroFullInfoType) => item.id === id) ?? null
-    });
+    async (id:number) => await superheroesApi.getById(id))
 
 
 
