ba5944a36dafedf0b459ecfef3403de0cd43f45a
Maksym <qlowlight@gmail.com>
2026-01-17 14:18:37 +0200

    add full and summary superheroes document types

---
+++ backend/src/modules/superheroes/infrastructure/types/sumerhero-full.document.type.ts
@@ -0,0 +1,10 @@
+export type SuperheroFullDocumentType = {
+  id: number;
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  super_power: string;
+  catch_phrase: string;
+  image_url: string;
+  created_at: Date;
+};
---
+++ backend/src/modules/superheroes/infrastructure/types/sumerhero-summary.document.type.ts
@@ -0,0 +1,6 @@
+export type SuperheroSummaryDocumentType = {
+  id: number;
+  nickname: string;
+  image_url: string;
+  created_at: Date;
+};
