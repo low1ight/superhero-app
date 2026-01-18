95bdee6190507c690b69ec472d8421a819af4253
Maksym <qlowlight@gmail.com>
2026-01-18 16:26:05 +0200

    add onDelete, add link to edit

--- frontend/src/pages/HeroInfoPage.tsx
+++ frontend/src/pages/HeroInfoPage.tsx
@@ -1,11 +1,12 @@
 import {Link, useNavigate, useParams} from "react-router-dom";
 import Button from "../components/ui/Button.tsx";
 import {useAppDispatch, useAppSelector} from "../store/hooks.ts";
-import {getHeroById} from "../store/heroesSlice.ts";
+import {deleteHero, getHeroById} from "../store/heroesSlice.ts";
 import {useEffect, useState} from "react";
 import {ConfirmModal} from "../components/ui/ConfirmModal.tsx";
 import HeroInfo from "../components/heroes/HeroInfo.tsx";
 import HeroInfoSkeleton from "../components/heroes/HeroInfoSekeleton.tsx";
+import BackToList from "../components/ui/BackToList.tsx";
 
 function HeroesInfoPage() {
 
@@ -16,57 +17,56 @@ function HeroesInfoPage() {
 
     const navigate = useNavigate();
 
-    const {isLoading,selected} = useAppSelector((state) => state.heroes);
+    const {isLoading, selected} = useAppSelector((state) => state.heroes);
 
 
     const dispatch = useAppDispatch();
 
     const onDelete = async () => {
-        if(!selected) return;
+        if (!selected) return;
 
-        console.log('delete')
-
+        await dispatch(deleteHero(heroId))
         setOpen(false)
         navigate('/heroes')
 
     }
 
 
-   useEffect(() => {
-       dispatch(getHeroById(heroId))
-   },[dispatch,heroId])
-
+    useEffect(() => {
+        dispatch(getHeroById(heroId))
+    }, [dispatch, heroId])
 
 
     return (
 
         <div className="space-y-4">
             <nav className="flex items-center justify-between">
-                <Link to="/heroes" className="inline-block text-blue-600 hover:underline">
-                    ← Return to the list
-                </Link>
+                <BackToList/>
                 <div>
-                    <Button type='button' disabled={isLoading} variant='secondary' className='mx-1.5'>
-                        Edit
-                    </Button>
-                    <Button type='button' onClick={() => setOpen(true)} disabled={isLoading} variant='danger'>
+                    <Link to={`/heroes/${id}/edit`}>
+                        <Button type='button' buttonName="Edit" disabled={isLoading} variant='secondary'
+                                className='mx-1.5'>
+                            Edit
+                        </Button>
+                    </Link>
+                    <Button type='button' buttonName="Delete" onClick={() => setOpen(true)} disabled={isLoading}
+                            variant='danger'>
                         Delete
                     </Button>
                 </div>
             </nav>
 
 
-
-                <ConfirmModal
-                    open={open}
-                    title="Delete the Hero?"
-                    description="This action cannot be undone."
-                    confirmText="Delete"
-                    cancelText="Cancel"
-                    loading={isLoading}
-                    onClose={() => setOpen(false)}
-                    onConfirm={onDelete}
-                />
+            <ConfirmModal
+                open={open}
+                title="Delete the Hero?"
+                description="This action cannot be undone."
+                confirmText="Delete"
+                cancelText="Cancel"
+                loading={isLoading}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+            />
 
             {isLoading ?
                 <HeroInfoSkeleton/> :
