14acbcbe76e4da261b1782bde860976644e23dac
Maksym <qlowlight@gmail.com>
2026-01-18 10:10:24 +0200

    add update-hero dto

---
+++ backend/src/modules/superheroes/dto/update-superhero.dto.ts
@@ -0,0 +1,8 @@
+export type UpdateSuperheroDto = {
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  super_power: string;
+  catch_phrase: string;
+  image_url: string;
+};
